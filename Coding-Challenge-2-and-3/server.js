11c988e6c3342789b27f55cc02f96c3e31928de3
AndyO97 <47606055+AndyO97@users.noreply.github.com>
2020-05-08 10:59:00 -0400

    Add files via upload

--- Coding-Challenge-2-and-3/server.js
+++ Coding-Challenge-2-and-3/server.js
@@ -4,10 +4,42 @@ const mongoose = require( 'mongoose' );
 const jsonParser = bodyParser.json();
 const { DATABASE_URL, PORT } = require( './config' );
 
+
 const app = express();
 
 
 /* Your code goes here */
+app.delete('/sports/delete', jsonParser, (req, res) =>{
+
+    let id = req.query.sportId;
+    let id2 = req.body.id;
+
+    if(!id2){
+        res.statusMessage ="There is no id on the body of the request";
+        return res.status(406).end();
+    }
+    if(!id){
+        res.statusMessage ="There is no id on the parameters";
+        return res.status(406).end();
+    }
+
+    if(id != id2){
+        res.statusMessage = `The id on the body and the parameters dont match`;
+        return res.status(409).end();
+    }
+    
+
+    SportController
+        .remove(id)
+        .then( result => {
+            return res.status(204);
+        })
+        .catch(error =>{
+            console.log(error);
+            return res.status(500);
+        })
+
+})
 
 
 app.listen( PORT, () => {
