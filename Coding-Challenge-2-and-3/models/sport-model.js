38dbe7cfa61e6e37d814378097483e1fec7f320d
AndyO97 <47606055+AndyO97@users.noreply.github.com>
2020-05-08 10:58:03 -0400

    Add files via upload

--- Coding-Challenge-2-and-3/models/sport-model.js
+++ Coding-Challenge-2-and-3/models/sport-model.js
@@ -2,7 +2,26 @@ const mongoose = require( 'mongoose' );
 const uuid = require( 'uuid' );
 
 /* Your code goes here */
+let sportCollection = mongoose.Schema({
+    id: uuid.v4(),
+    name: String,
+    num_players: Number
+})
+
+let Sports = mongoose.model('sport', sportCollection);
+
+let SportController = {
+    remove: function(id){
+        return Sports.findOneAndRemove({id: id})
+        .then( result =>{
+            return result;
+        })
+        .catch(error =>{
+            throw Error(eror);
+        })
+    }
+}
 
 module.exports = {
-    
+    SportController
 };
